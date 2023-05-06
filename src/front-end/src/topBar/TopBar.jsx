eabf25d2f2b78a0231afc59bae955d3aff310175
jose-cleiton <cleitons835@gmail.com>
2023-05-05 22:47:05 -0300

    feat: :sparkles: organizing the top

--- src/front-end/src/topBar/TopBar.jsx
+++ src/front-end/src/topBar/TopBar.jsx
@@ -1,10 +1,32 @@
 import "./topBar.css"
+
 const TopBar = () => {
     return (
         <div className="top">
-            TopBar
+            <div className="topLeft">
+                <i className="topIcon fab fa-facebook-square"></i>
+                <i className="topIcon fab fa-instagram-square"></i>
+                <i className="topIcon fab fa-pinterest-square"></i>
+                <i className="topIcon fab fa-twitter-square"></i>
+            </div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">HOME</li>
+                    <li className="topListItem">ABOUT</li>
+                    <li className="topListItem">CONTACT</li>
+                    <li className="topListItem">WRITE</li>
+                    <li className="topListItem">LOGOUT</li>
+                </ul>
+            </div>
+            <div className="topRight">
+            <img
+            className="topImg"
+            src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+                />
+                <i className="topSearchIcon fas fa-search"></i>     
+    </div>                
         </div>
-
     );
 };
  
